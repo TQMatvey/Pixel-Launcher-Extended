fa2367b40214ff7734138537bcc11aac12ebaf65
TQMatvey <maybeetube@gmail.com>
2023-06-17 10:44:33 +0700

    remove manual installer

--- builder_ci.sh
+++ builder_ci.sh
@@ -4,7 +4,7 @@
 version=$(grep "version=" module.prop | cut -d "=" -f 2)
 
 # Delete existing installers
-rm -rf "Pixel Launcher Extended Offline Installer $version.zip"
+rm -rf "Pixel Launcher Extended*.zip"
 
 # Create Offline Installer
 echo ">> Creating Offline Installer"
@@ -15,11 +15,5 @@ rm setup.sh
 # Create Online Installer
 echo ">> Creating Online Installer"
 cp online_setup.sh setup.sh
-zip -r -q "Pixel Launcher Extended Online Installer $version.zip" . -x .git/\* screenshots/\* builder.sh builder_dependencies.sh banner.jpg banner2.jpg changelog.md codename.txt logo.png online_setup.sh customize_setup.sh manual_setup.sh manual_installer_instructions.md README.md Pixel\ Launcher\ Extended*
-rm setup.sh
-
-# Create Manual Installer
-echo ">> Creating Online Installer"
-cp manual_setup.sh setup.sh
-zip -r -q "Pixel Launcher Extended Manual Installer $version.zip" . -x .git/\* screenshots/\* builder.sh builder_dependencies.sh banner.jpg banner2.jpg changelog.md codename.txt logo.png online_setup.sh customize_setup.sh manual_setup.sh manual_installer_instructions.md README.md Pixel\ Launcher\ Extended*
+zip -r -q "Pixel Launcher Extended Online Installer $version.zip" . -x .git/\* Modifications/\* screenshots/\* builder.sh builder_dependencies.sh banner.jpg banner2.jpg changelog.md codename.txt logo.png offline_setup.sh customize_setup.sh online_setup.sh manual_setup.sh manual_installer_instructions.md config.txt README.md system/product/priv-app/NexusLauncherRelease/*\* system/product/priv-app/PixelLauncherMods/PixelLauncherMods.apk system/product/overlay/ThemedIconsOverlay/*\* system/system_ext/priv-app/WallpaperPickerGoogleRelease/* system/product/overlay/TeamFiles* system/product/priv-app/ExtendedSettings/ExtendedSettings.apk system/product/priv-app/IconShapeChanger/IconShapeChanger.apk Pixel\ Launcher\ Extended*
 rm setup.sh
